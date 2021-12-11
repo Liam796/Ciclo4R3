8a757a74304837141d277a030589c0b448eabc1c
Liam796 <william.martinez1.mt@correo.usa.edu.co>
2021-12-11 18:31:26 -0500

    first commit

---
+++ src/main/java/com/retodos/repository/OrderRepository.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.retodos.repository;
+
+import com.retodos.model.Order;
+import com.retodos.repository.crud.OrderCrudRepository;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author elber
+ */
+@Repository
+public class OrderRepository {
+    
+    @Autowired
+    private OrderCrudRepository orderCrudRepository;
+
+    public List<Order> getAll() {
+        return (List<Order>) orderCrudRepository.findAll();
+    }
+
+    public Optional<Order> getOrder(int id) {
+        return orderCrudRepository.findById(id);
+    }
+
+    public Order create(Order order) {
+        return orderCrudRepository.save(order);
+    }
+
+    public void update(Order order) {
+        orderCrudRepository.save(order);
+    }
+
+    public void delete(Order order) {
+        orderCrudRepository.delete(order);
+    }
+    
+    public Optional<Order> lastUserId(){
+        return orderCrudRepository.findTopByOrderByIdDesc();
+    }
+    
+    public List<Order> findByZone(String zona) {
+        return orderCrudRepository.findByZone(zona);
+    }
+}
